8629cce7c0546dca77c8244cd0b9b37a62c39ed5
Ngari Ndung'u <ngari.ndungu@gmail.com>
2018-08-04 22:56:27 +0300

    make landscape layout functional

--- app/src/main/java/com/createchs/scorekeeper/MainActivity.java
+++ app/src/main/java/com/createchs/scorekeeper/MainActivity.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends Activity {
 
     private int scoreTeamA;
     private int scoreTeamB;
+    // constants for storing state
     private static final String TEAM_A = "scoreTeamA";
     private static final String TEAM_B = "scoreTeamB";
+    // Team A button ids
+    private ArrayList<Integer> teamAButtons = new ArrayList<>();
 
 
 
@@ -20,6 +25,10 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         scoreTeamA = scoreTeamB = 0;
+        // populate teamAButtons
+        teamAButtons.add(R.id.try_team_a);
+        teamAButtons.add(R.id.converted_team_a);
+        teamAButtons.add(R.id.penalty_team_a);
     }
 
     /*
@@ -47,12 +56,17 @@ public class MainActivity extends Activity {
         super.onSaveInstanceState(outState);
     }
 
-
+    /*
+    * reset score for both teams to zero and update the displayed scores
+    */
     public void resetScores(View view) {
         scoreTeamA = scoreTeamB = 0;
         display();
     }
 
+    /*
+    * Update displayed scores
+    */
     private void display() {
         TextView teamAScore = (TextView) findViewById(R.id.score_team_a);
         TextView teamBScore = (TextView) findViewById(R.id.score_team_b);
@@ -61,25 +75,31 @@ public class MainActivity extends Activity {
         teamBScore.setText(String.valueOf(scoreTeamB));
     }
 
-    public void addTry(View view) {
-        addScore(view,5);
-        display();
-    }
-
+    /*
+    * Add to the current score
+    */
     private void addScore(View view, int score) {
-        if (view.getParent() == findViewById(R.id.interactives_team_a)) {
+        if (teamAButtons.contains(view.getId())) {
             scoreTeamA += score;
         } else {
             scoreTeamB += score;
         }
     }
 
+    public void addTry(View view) {
+        // 5 points for a try
+        addScore(view,5);
+        display();
+    }
+
     public void addConvertedTry(View view) {
+        // 7 points for a try + conversion
         addScore(view,7);
         display();
     }
 
     public void addPenalty(View view) {
+        // 3 points for a penalty
         addScore(view,3);
         display();
     }
